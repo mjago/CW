8318e16daaaa429793f52827474bd83265b0bd10
mjago <martyn.jago@btinternet.com>
2016-05-30 12:58:52 +0100

    module Element

--- lib/cw/cw_stream.rb
+++ lib/cw/cw_stream.rb
@@ -2,6 +2,8 @@
 
 class CwStream
 
+  include Element
+
   attr_accessor :active_region
 
   def initialize
@@ -11,10 +13,6 @@ class CwStream
 
   def empty
     @stream, @success, @first_element, @last_element = {},{}, 0, 0
-  end
-
-  def count
-    @last_element - @first_element
   end
 
   def add_word word
@@ -27,14 +25,6 @@ class CwStream
     @stream[@last_element] = char
     @success[@last_element] = nil
     inc_last_element
-  end
-
-  def inc_last_element
-    @last_element += 1
-  end
-
-  def inc_first_element
-    @first_element += 1
   end
 
   def mark(element, type)
@@ -113,61 +103,4 @@ class CwStream
     end
   end
 
-  def element type
-    return @last_element - 1 if type == :last
-    first = @last_element - @active_region - 1
-    first < 0 ? 0 : first
-  end
-
-  def match_first_active_element match
-    if ! stream_empty?
-      found = false
-      first = element(:first)
-      first.upto element(:last) do |ele|
-        if found
-          first.upto found - 1 do |failed|
-            @success[failed] = false # unless @success[ele]
-          end
-          break
-        elsif((@stream[ele] == match) && (! @success[ele]))
-          @success[ele], found = true, ele
-        else
-          @success[first] = false
-        end
-      end
-    end
-  end
-
-  def match_last_active_element(match)
-    process_last_active_element(match) unless stream_empty?
-  end
-
-  def process_last_active_element(match)
-    first = get_first
-    last  = get_last
-    check_last_element_success(match, first, last)
-  end
-
-  def check_last_element_success(match, first, last)
-    found = false
-    last.downto(first) do |element|
-      if found
-        @success[element] = false unless @success[element]
-      elsif((@stream[element] == match) && (! @success[element]))
-        @success[element], found = true, true
-      else
-        @success[first] = false
-      end
-    end
-  end
-
-  def get_first
-    first = @last_element - @active_region - 1
-    first = 0 if(first < 0)
-    first
-  end
-
-  def get_last
-    @last_element - 1
-  end
 end
---
+++ lib/cw/element.rb
@@ -0,0 +1,73 @@
+module Element
+
+  def element type
+    return @last_element - 1 if type == :last
+    first = @last_element - @active_region - 1
+    first < 0 ? 0 : first
+  end
+
+  def match_first_active_element match
+    if ! stream_empty?
+      found = false
+      first = element(:first)
+      first.upto element(:last) do |ele|
+        if found
+          first.upto found - 1 do |failed|
+            @success[failed] = false # unless @success[ele]
+          end
+          break
+        elsif((@stream[ele] == match) && (! @success[ele]))
+          @success[ele], found = true, ele
+        else
+          @success[first] = false
+        end
+      end
+    end
+  end
+
+  def match_last_active_element(match)
+    process_last_active_element(match) unless stream_empty?
+  end
+
+  def process_last_active_element(match)
+    first = get_first
+    last  = get_last
+    check_last_element_success(match, first, last)
+  end
+
+  def check_last_element_success(match, first, last)
+    found = false
+    last.downto(first) do |element|
+      if found
+        @success[element] = false unless @success[element]
+      elsif((@stream[element] == match) && (! @success[element]))
+        @success[element], found = true, true
+      else
+        @success[first] = false
+      end
+    end
+  end
+
+  def get_first
+    first = @last_element - @active_region - 1
+    first = 0 if(first < 0)
+    first
+  end
+
+  def get_last
+    @last_element - 1
+  end
+
+  def count
+    @last_element - @first_element
+  end
+
+  def inc_last_element
+    @last_element += 1
+  end
+
+  def inc_first_element
+    @first_element += 1
+  end
+
+end
