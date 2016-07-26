c38a88e269453371f71214525e068917fc1587ce
mjago <martyn.jago@btinternet.com>
2016-07-26 11:26:52 +0100

    Document no_noise command

--- cw_scripts/no_noise.rb
+++ cw_scripts/no_noise.rb
@@ -1,9 +1,10 @@
 require "cw"
 
-cw = CW.new
-cw.use_ebook2cw
-cw.noise
-cw.word_count 2
-cw.test_letters
-cw.no_noise
-cw.test_letters
+cw do
+  use_ebook2cw
+  noise
+  word_count 2
+  test_letters
+  no_noise
+  test_letters
+end
