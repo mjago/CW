e9acb389f9d1bca9494a90a4b9f3f547449c2be0
mjago <martyn.jago@btinternet.com>
2016-08-06 22:19:35 +0100

    Update example script

--- example.rb
+++ example.rb
@@ -1,136 +1,138 @@
 require 'cw'
 
 cw do
-  comment 'read book feed (1 sentence)'
+  comment 'test alphabet vowels'
+  wpm 15
+  load_vowels
+  shuffle
+end
+
+cw do
+  comment 'test letters a..m'
+  wpm 18
+  load_alphabet
+  containing('a'..'m')
+  shuffle
+end
+
+cw do
+  comment 'test 8 words made with letters a..m - test by letter'
+  wpm 18
+  containing('a'..'m')
+  shuffle
+  word_count 8
+end
+
+cw do
+  comment 'test letters n..z'
+  wpm 18
+  load_alphabet
+  containing('n'..'z')
+  shuffle
+end
+
+cw do
+  comment 'test 8 words made with letters n..z - test by word'
+  wpm 18
+  containing('n'..'z')
+  shuffle
+  word_count 8
+  test_words
+end
+
+cw do
+  comment 'test numbers'
+  wpm 20
+  load_numbers
+  shuffle
+end
+
+cw do
+  comment 'test alphabet - repeat until correct'
+  wpm 25
+  load_alphabet
+  shuffle
+  repeat_word
+end
+
+cw do
+  comment 'test 8 most common words no longer than 4 letters'
+  wpm 20
+  load_most_common_words
+  shuffle
+  no_longer_than 4
+  word_count 8
+end
+
+cw do
+  comment 'test 8 words including letter sequence "ing"'
+  shuffle
+  including('ing')
+  word_size 6
+end
+
+cw do
+  comment 'test 8 words having 6 letters - play each word twice'
+  shuffle
+  word_size 6
+  word_count 8
+  double_words
+end
+
+cw do
+  comment 'test 8 words beginning with "qu" - repeat whole sequence once'
+  wpm 20
+  shuffle
+  word_count 8
+  repeat 1
+end
+
+cw do
+  comment 'test 8 words ending with "tion" - test by word'
+  wpm 15
+  shuffle
+  ending_with 'tion'
+  word_count 8
+  test_words
+end
+
+cw do
+  comment 'read one sentence of book'
+  wpm 20
   read_book(sentences: 1)
 end
 
 cw do
-  comment 'read book feed (1 minute)'
-  read_book(duration: 1)
+  comment 'read rss feed (quote of the day)'
+  wpm 18
+  read_rss(:quotation, 1)
 end
 
 cw do
-  comment 'read rss feed (1 article)'
-  read_rss(:reuters, 1)
+  comment 'test 6 common cw abbreviations'
+   wpm  15
+   load_abbreviations
+   shuffle
+   word_count 6
 end
 
 cw do
-  name 'test straight alphabet'
-  alphabet
-end
+  comment "test 8 Q codes by ear (no keyboard test)"
+   wpm  20
+   load_codes
+   shuffle
+   word_count 8
+   print_words
+ end
 
 cw do
-  comment 'test straight numbers'
-  numbers
-end
+  comment "test 8 words by ear - reveal words at end of test"
+   wpm  20
+   shuffle
+   word_count 8
+   reveal
+ end
 
-cw do
-  wpm  18
-  ewpm 12
-  load_abbreviations
-  shuffle
-end
-
-cw do
-  wpm  18
-  ewpm 12
-  load_q_codes
-  shuffle
-end
-
-def cw_settings
-  shuffle
-  wpm 20
-  ewpm 15
-  #  use_ebook2cw
-  use_ruby_tone
-  word_count 5
-  word_size 4
-  print_letters
-  puts self.to_s
-end
-
-cw do
-  comment 'test random letters'
-  random_letters(size: 4)
-  cw_settings
-end
-
-cw do
-  comment 'test random numbers'
-#  use_ebook2cw
-  cw_settings
-  random_numbers(count: 2, size: 5)
-end
-
-cw do
-  comment 'test random letters numbers'
-#  use_ebook2cw
-  cw_settings
-  random_letters_numbers(count: 2, size: 11)
-end
-
-wpm = 15
-ewpm = 12
-loop do
-
-  test = cw
-#  test.use_ebook2cw
-  test.comment 'test words beginning with b'
-  test.shuffle
-  test.wpm             wpm
-  test.effective_wpm   ewpm
-  test.beginning_with  'b'
-  test.word_size       4
-  test.word_count      4
-  puts test.to_s
-  test.test_words
-
-  test = cw
-  test.comment 'test words including ing'
-#  test.use_ebook2cw
-  test.shuffle
-  test.wpm             wpm
-  test.effective_wpm   ewpm
-  test.including       'ing'
-  test.word_count      5
-  test.test_words
-
-  #  test = cw
-  #  test.comment         'test ing'
-  #  test.use_ebook2cw
-  ##  test.shuffle
-  #  test.wpm             wpm
-  #  test.effective_wpm   ewpm
-  #  test.beginning_with ['f']
-  #  test.word_size       4
-  #  test.word_count      5
-  #  test.test_words
-  #
-  #  test = cw
-  #  test.comment         'test ing'
-  #  test.use_ruby_tone
-  ##  test.shuffle
-  #  test.wpm             wpm
-  #  test.effective_wpm   ewpm
-  #  test.beginning_with ['f']
-  #  test.word_size       4
-  #  test.word_count      5
-  #  test.test_words
-
-  #  test = cw
-  #  test.comment         'test ing'
-  #  test.shuffle
-  #  test.wpm             wpm
-  #  test.effective_wpm   ewpm
-  #  test.beginning_with ['j']
-  #  test.word_size       4
-  #  test.word_count      5
-  #  test.test_words
-  #
-  wpm += 2
-  break if wpm >= 24
-end
+# See documentation for more details - and more commands!
 
 puts 'done'
