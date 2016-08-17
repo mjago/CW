6b7c4d2e08936189e8a01b3f02caa76c0f0e5c49
mjago <martyn.jago@btinternet.com>
2016-08-17 08:26:42 +0100

    Load encodings from yaml

--- lib/cw/cw_encoding.rb
+++ lib/cw/cw_encoding.rb
@@ -4,63 +4,68 @@ module CWG
 
   class CwEncoding
 
-    ENCODINGS = {
-      'a' => [:dot, :dash],
-      'b' => [:dash, :dot, :dot, :dot],
-      'c' => [:dash, :dot, :dash, :dot],
-      'd' => [:dash, :dot, :dot],
-      'e' => [:dot],
-      'f' => [:dot, :dot, :dash, :dot],
-      'g' => [:dash, :dash, :dot],
-      'h' => [:dot, :dot, :dot, :dot],
-      'i' => [:dot, :dot],
-      'j' => [:dot, :dash, :dash, :dash],
-      'k' => [:dash, :dot, :dash],
-      'l' => [:dot, :dash, :dot, :dot],
-      'm' => [:dash, :dash],
-      'n' => [:dash, :dot],
-      'o' => [:dash, :dash, :dash],
-      'p' => [:dot, :dash, :dash, :dot],
-      'q' => [:dash, :dash, :dot, :dash],
-      'r' => [:dot, :dash, :dot],
-      's' => [:dot, :dot, :dot],
-      't' => [:dash],
-      'u' => [:dot, :dot, :dash],
-      'v' => [:dot, :dot, :dot, :dash],
-      'w' => [:dot, :dash, :dash],
-      'x' => [:dash, :dot, :dot, :dash],
-      'y' => [:dash, :dot, :dash, :dash],
-      'z' => [:dash, :dash, :dot, :dot],
-      '1' => [:dot, :dash, :dash, :dash, :dash],
-      '2' => [:dot, :dot, :dash, :dash, :dash],
-      '3' => [:dot, :dot, :dot, :dash, :dash],
-      '4' => [:dot, :dot, :dot, :dot, :dash],
-      '5' => [:dot, :dot, :dot, :dot, :dot],
-      '6' => [:dash, :dot, :dot, :dot, :dot],
-      '7' => [:dash, :dash, :dot, :dot, :dot],
-      '8' => [:dash, :dash, :dash, :dot, :dot],
-      '9' => [:dash, :dash, :dash, :dash, :dot],
-      '0' => [:dash, :dash, :dash, :dash, :dash],
-      '.' => [:dot, :dash, :dot, :dash, :dot, :dash],
-      ',' => [:dash, :dash, :dot, :dot, :dash, :dash],
-      '=' => [:dash, :dot, :dot,:dot, :dash],
-      '!' => [:dot, :dot, :dash, :dash, :dot],
-      '/' => [:dash, :dot, :dot, :dash, :dot],
-      '?' => [:dot, :dot, :dash, :dash, :dot, :dot],
-      ' ' => []
-    }
+    include FileDetails
 
-    def fetch char
-      ENCODINGS[char]
-    end
+#    ENCODINGS = {
+#      'a' => [:dot, :dash],
+#      'b' => [:dash, :dot, :dot, :dot],
+#      'c' => [:dash, :dot, :dash, :dot],
+#      'd' => [:dash, :dot, :dot],
+#      'e' => [:dot],
+#      'f' => [:dot, :dot, :dash, :dot],
+#      'g' => [:dash, :dash, :dot],
+#      'h' => [:dot, :dot, :dot, :dot],
+#      'i' => [:dot, :dot],
+#      'j' => [:dot, :dash, :dash, :dash],
+#      'k' => [:dash, :dot, :dash],
+#      'l' => [:dot, :dash, :dot, :dot],
+#      'm' => [:dash, :dash],
+#      'n' => [:dash, :dot],
+#      'o' => [:dash, :dash, :dash],
+#      'p' => [:dot, :dash, :dash, :dot],
+#      'q' => [:dash, :dash, :dot, :dash],
+#      'r' => [:dot, :dash, :dot],
+#      's' => [:dot, :dot, :dot],
+#      't' => [:dash],
+#      'u' => [:dot, :dot, :dash],
+#      'v' => [:dot, :dot, :dot, :dash],
+#      'w' => [:dot, :dash, :dash],
+#      'x' => [:dash, :dot, :dot, :dash],
+#      'y' => [:dash, :dot, :dash, :dash],
+#      'z' => [:dash, :dash, :dot, :dot],
+#      '1' => [:dot, :dash, :dash, :dash, :dash],
+#      '2' => [:dot, :dot, :dash, :dash, :dash],
+#      '3' => [:dot, :dot, :dot, :dash, :dash],
+#      '4' => [:dot, :dot, :dot, :dot, :dash],
+#      '5' => [:dot, :dot, :dot, :dot, :dot],
+#      '6' => [:dash, :dot, :dot, :dot, :dot],
+#      '7' => [:dash, :dash, :dot, :dot, :dot],
+#      '8' => [:dash, :dash, :dash, :dot, :dot],
+#      '9' => [:dash, :dash, :dash, :dash, :dot],
+#      '0' => [:dash, :dash, :dash, :dash, :dash],
+#      '.' => [:dot, :dash, :dot, :dash, :dot, :dash],
+#      ',' => [:dash, :dash, :dot, :dot, :dash, :dash],
+#      '=' => [:dash, :dot, :dot,:dot, :dash],
+#      '!' => [:dot, :dot, :dash, :dash, :dot],
+#      '/' => [:dash, :dot, :dot, :dash, :dot],
+#      '?' => [:dot, :dot, :dash, :dash, :dot, :dot],
+#      ' ' => []
+#    }
 
     def encodings
-      ENCODINGS
+      if @encodings.nil?
+        @encodings = load_code
+      end
+      @encodings
+    end
+
+    def fetch char
+      encodings[char]
     end
 
     def match_elements arg
       chars = []
-      ENCODINGS.each_pair do |key, value|
+      encodings.each_pair do |key, value|
         chars << key unless value.include?(:dot) if arg[0]  == :dashes
         chars << key unless value.include?(:dash) if arg[0] == :dots
         chars << key if (value.size < arg[1] )if arg[0]     == :less_than
@@ -69,6 +74,12 @@ module CWG
         chars.delete(' ')
       end
       chars
+    end
+
+    def load_code
+      File.open(CODE_FILENAME, "r") do |code|
+        YAML::load(code)
+      end
     end
   end
 end
--- lib/cw/file_details.rb
+++ lib/cw/file_details.rb
@@ -5,8 +5,10 @@ module CWG
   module FileDetails
     HERE  = File.dirname(__FILE__)
     ROOT  = File.expand_path File.join(HERE,'..','..')
-    TEXT  = File.join(ROOT,'data','text')
+    DATA  = File.join(ROOT,'data')
     AUDIO = File.join(ROOT,'audio')
+    TEXT  = File.join(DATA,'text')
+    CODE  = File.join(DATA,'code')
 
     ENGLISH_DICT     = File.join TEXT, "english.txt"
     ABBREVIATIONS    = File.join TEXT, "abbreviations.txt"
@@ -16,16 +18,13 @@ module CWG
     SPACE_FILENAME   = File.join AUDIO, "space.wav"
     E_SPACE_FILENAME = File.join AUDIO, "e_space.wav"
     CONFIG_FILENAME  = File.join ROOT, ".cw_config"
+    CODE_FILENAME    = File.join CODE, "code.yaml"
 
     def init_filenames
       @repeat_tone   = File.join(AUDIO, "rpt.mp3")
       @r_tone        = File.join(AUDIO, "r.mp3")
       @text_folder   = TEXT
       @progress_file = 'progress.txt'
-    end
-
-    def expand path
-      File.expand_path path
     end
   end
 end
